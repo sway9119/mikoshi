08b060da854a37a379008d8c055a16525adfceae
Yoshiki Hiraba <hiraba09@gmail.com>
2019-02-03 16:29:31 +0900

    homeコントローラーの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # root 'root#index'
+  root to: 'home#index'
   resources :hulus
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
